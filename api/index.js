cbadfd7667286fda4ac958d0cd0add6e41293a02
safeer <safeersifarath@gmail.com>
2024-10-13 03:15:51 +0530

    create home page

--- api/index.js
+++ api/index.js
@@ -1,15 +1,19 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from "express";
+import mongoose from "mongoose";
 
-mongoose.connect('mongodb+srv://safeer:7989@cook-with-shaki.btw6x.mongodb.net/cook-with-shaki?retryWrites=true&w=majority&appName=cook-with-shaki'
-    
-)
-.then(() => {
-    console.log('MongoDb is connected')
-})
+mongoose
+  .connect(
+    "mongodb+srv://safeer:7989@cook-with-shaki.btw6x.mongodb.net/cook-with-shaki?retryWrites=true&w=majority&appName=cook-with-shaki"
+  )
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const app = express()
+const app = express();
 
-app.listen(3000, () =>{
-    console.log('sever is running on port 3000')
-})
+app.listen(3000, () => {
+  console.log("sever is running on port 3000");
+});
